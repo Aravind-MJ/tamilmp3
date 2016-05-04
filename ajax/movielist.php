526e14fcbcf7aaac557c1c596adeceef67c99bf8
jayalakshmipv <jayalakshmivjvj@gmail.com>
2016-05-04 12:23:57 +0530

    featured album

--- ajax/movielist.php
+++ ajax/movielist.php
@@ -3,21 +3,35 @@
 $file = "../text_files/featuredAlbum.txt";
 
 $response = array();
-$ofile = fopen($file, 'r') or die("Unable to open file!");
-while ($line = fgets($ofile)) {
-    preg_match_all('/[0-9]{4}/', $line, $iyear);
-    preg_match_all('/[A-Za-z ]+/', $line, $iname);
-//    preg_match_all('[0-9]{4}+/[A-Za-z ]/', $line, $istar);
-    $response[$iname[0][0]] = new stdClass;
-    $response[$iname[0][0]]->name = $iname[0][0];
-//    $response[$iname[0][0]]->star = $istar[0][0];
-    if (isset($iyear[0][0])) {
-        $response[$iname[0][0]]->year = $iyear[0][0];
-    } else {
-        $response[$iname[0][0]]->year = null;
-    }
 
-}
+    $fopen = fopen($file, 'r') or die("Unable to open file!");
+
+    $fread = fread($fopen,filesize($file));
+
+    fclose($fopen);
+
+    $remove = "\n";
+
+    $split = explode($remove, $fread);
+    
+    $del = ':';
+
+    foreach ($split as $string)
+    {
+        
+        $value = explode($del, $string);
+        $rowArray = array(
+                'name'  => isset($value[0]) ? $value[0] : null,
+                'year'  => isset($value[1]) ? $value[1] : null,
+                'star'  => isset($value[2]) ? $value[2] : null,
+                'music'  => isset($value[3]) ? $value[3] : null,
+                'director'  => isset($value[4]) ? $value[4] : null,
+            );
+        
+        array_push($response,$rowArray);
+
+    }
+ array_pop($response);
 $response = array($response);
 echo json_encode($response);
 
