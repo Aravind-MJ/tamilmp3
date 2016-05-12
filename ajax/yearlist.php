2df789be7f9e90e2b974a0e9f287e41db9c69c74
jayalakshmipv <jayalakshmivjvj@gmail.com>
2016-05-12 10:39:45 +0530

    movie year list

--- ajax/yearlist.php
+++ ajax/yearlist.php
@@ -3,7 +3,6 @@
 $param = json_decode(file_get_contents("php://input"));
 $folder = $param->loc;
 
-
 function songslist($strdir) {
     $txtyears = array();
     if (is_dir($strdir)) {
@@ -12,14 +11,17 @@ function songslist($strdir) {
             $files = explode('/', $file);
             $txtyears[$files[sizeof($files) - 1]]['name'] = $files[sizeof($files) - 1];
             $txtyears[$files[sizeof($files) - 1]]['name'] = preg_replace('/\\.[^.\\s]{3,4}$/', '', $txtyears[$files[sizeof($files) - 1]]['name']);
+            if (!is_numeric($txtyears[$files[sizeof($files) - 1]]['name'])) {
+                unset($txtyears[$files[sizeof($files) - 1]]);
+            }
         }
     }
     return($txtyears);
 }
 
 $years = songslist($folder);
-$years = array_reverse($years, true); 
-$count = ceil(sizeof($years)/4);
-$years = array_chunk($years,$count);
+$years = array_reverse($years, true);
+$count = ceil(sizeof($years) / 4);
+$years = array_chunk($years, $count);
 echo json_encode($years);
 
