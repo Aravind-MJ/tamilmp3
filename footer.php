e2fd076e07899397a05fb981fff07069f95da827
aravindmjgit <aravindnair93@gmail.com>
2016-05-23 15:51:37 +0530

    Footer

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
             <div class="row">
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="footer_bottom_box">
-                        <p> Copyright &copy;  2016 Friends Tamil Mp3. Designed by Imorkraft Solutions. All Rights Reserved. </p>
+                        <p> Copyright &copy;  2016 Friends Tamil Mp3. Designed by<a href="https://www.imrokraft.com" target="_SELF"> Imrokraft Solutions</a>. All Rights Reserved. </p>
                     </div>
                 </div>
             </div>
