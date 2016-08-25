e69e2f5f344f3b615e804248dcdc28b5f2180af1
subin08 <subin@imrokraft.com>
2016-08-25 14:53:49 +0530

    about Us changes

--- template/aboutus.php
+++ template/aboutus.php
@@ -1,1 +1,17 @@
+<div class="col-md-12 col-sm-12 col-xs-12">
+	<div class="about_us">
+	<h2> About Us</h2>
 
+<p>Welcome to friendstamilmp3, Music World. We are proud to introduce this new section that provides you with our ultimate free tamil mp3 song database. You can listen as well as download songs in this section.
+</p>
+<p>When you enter into the site you may have a question in mind, whether it might be regarding your favorite hits and those which you will be pleased to hear..!!
+</p>
+<p>So we have made great efforts to give you the best in quality and the largest collection as well.
+</p>
+<p>We have made this section interactive for the users, by simply making use of the comment menu you can place your requests for songs, or point out about any missing albums, so that we can add them too!!
+</p>
+<p>FTC Team is solely depending on your acknowledgement and support for us to work harder to give you the best in quality and collection. Kindly give us your feedback to help imrpove ourselves in any way that would please you.
+</p>
+
+	</div>
+</div>
