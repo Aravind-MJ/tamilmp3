88452dc7a321e9520e31f86196d4bd188ae8d1e7
subin08 <subin@imrokraft.com>
2016-08-31 10:08:52 +0530

    commant page design

--- template/comments.php
+++ template/comments.php
@@ -1,3 +1,22 @@
 <?php
-echo 'hai';
+
 ?>
+<div class="comment">
+<h2>Commant</h2>
+<p>In order to improve our performance we expect your comments, suggestions and feed back. You can also be the part of us in improving this site by sending valuable suggestions and requesting missing albums or songs.</p>
+</div>
+<div class="contact" id="contact">
+<div class="contact-grids">
+				
+				<div class="col-md-12 col-sm-12 col-xs-12 contact-form">
+					<form action="#" method="post">
+						<input type="text" name="First Name" placeholder="First Name" required="">
+						<input class="email" name="Location" type="text" placeholder="Location" required="">
+						<input type="text" name="Number" placeholder="Mobile Number" required="">
+						<input class="email" name="Email" type="text" placeholder="Email" required="">
+						<textarea name="Commant" placeholder="Commant" required=""></textarea>
+						<input type="submit" value="SUBMIT">
+					</form>
+				</div>
+				<div class="clearfix"> </div>	
+			</div></div>
