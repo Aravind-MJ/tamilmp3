5d58b20b96fbb1a4b6458c01d1d59c5995e34e76
Aravind M J <aravindnair93@gmail.com>
2016-11-14 16:42:29 +0530

    Changes

--- ajax/read.php
+++ ajax/read.php
@@ -1,14 +1,14 @@
 <?php
 
 $param = json_decode(file_get_contents("php://input"));
-$file = $param->file; 
+$file = $param->file;
 $col = $param->col;
 
 $response = array();
 $ofile = fopen($file, 'r') or die("Unable to open file!");
 while ($line = fgets($ofile)) {
     preg_match_all('/[\[][0-9][\]]{4}/', $line, $iyear);
-    preg_match_all('/[\(\)0-9A-Za-z ]+/', $line, $iname);
+    preg_match_all('/[\(\)0-9A-Za-z\.\+\- ]+/', $line, $iname);
     $response[$iname[0][0]] = new stdClass;
     $response[$iname[0][0]]->name = trim($iname[0][0]);
     if (isset($iyear[0][0])) {
@@ -18,8 +18,10 @@ while ($line = fgets($ofile)) {
     }
 }
 sort($response);
-$count = ceil(sizeof($response)/$col);
-$response = array_chunk($response,$count);
+$count = ceil(sizeof($response) / $col);
+if (sizeof($response) >= 15) {
+    $response = array_chunk($response, $count);
+}
 echo json_encode($response);
 
 
